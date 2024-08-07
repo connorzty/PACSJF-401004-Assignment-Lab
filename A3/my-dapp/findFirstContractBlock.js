e8d2eca678cbaa1bb48ef8150d3a0e609006a517
Connor <131134694+connorzty@users.noreply.github.com>
2024-08-07 10:04:10 +0800

    Comment

--- A3/my-dapp/findFirstContractBlock.js
+++ A3/my-dapp/findFirstContractBlock.js
@@ -1,11 +1,12 @@
 const {Web3} = require('web3');
-// Replace with your Infura project ID
-const infuraProjectId = 'd8a640f732874067bed901dfd5ee21f7';
-const infuraEndpoint = `https://mainnet.infura.io/v3/${infuraProjectId}`;
+
+const infuraProjectId = 'd8a640f732874067bed901dfd5ee21f7'; // my infura API
+const infuraEndpoint = `https://mainnet.infura.io/v3/${infuraProjectId}`; //Link with API
 
 const web3 = new Web3(new Web3.providers.HttpProvider(infuraEndpoint));
 
 async function findFirstContractCreationBlock() {
+    //Try to get the latest block and test for connection
     let latestBlockNumber;
     try {
         latestBlockNumber = await web3.eth.getBlockNumber();
@@ -14,7 +15,7 @@ async function findFirstContractCreationBlock() {
         console.error('Error fetching the latest block number:', error);
         return;
     }
-
+    //Getting first contract creation transaction
     for (let i = 0; i <= latestBlockNumber; i++) {
         try {
             let block = await web3.eth.getBlock(i, true);
@@ -28,7 +29,7 @@ async function findFirstContractCreationBlock() {
             console.error(`Error fetching block ${i}:`, error);
         }
     }
-
+    //Print in console
     console.log("No contract creation transaction found.");
     return null;
 }
